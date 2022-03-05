4da75f8de3bcbfb4878ce5ab73070c749887255d
Sang Shin <sangshinpassion@gmail.com>
2022-03-05 09:20:46 -0500

    refactored - extract methods

--- src/main/java/com/example/javawordcount/Main.java
+++ src/main/java/com/example/javawordcount/Main.java
@@ -9,28 +9,22 @@ import static com.example.javawordcount.Constants.unTextTest;
 public class Main {
 
     public static final int ONE = 1;
+    public static final String REGEX = "[[\\.\\?\\!\\,\\;\\:\\{\\}\\(\\)\\']]";
 
     public static void main(String[] args) {
 
         String unTextWithNoPunctuation
-                = unText.replaceAll("[[\\.\\?\\!\\,\\;\\:\\{\\}\\(\\)\\']]", "");
+                = unText.replaceAll(REGEX, "");
         String[] words = unTextWithNoPunctuation.split(" +");
 
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = constructMapFromWords(words);
+        ArrayList<Integer> values = createSortedArrayListFromValuesOfHashMap(map);
 
-        for (String word : words) {
-            word = word.toUpperCase();
-            boolean containsKey = map.containsKey(word);
-            if (containsKey) {
-                map.replace(word, map.get(word) + ONE);
-            } else {
-                map.put(word, ONE);
-            }
-        }
+        printTheWordsInHighestOccurencesFirstOrder(map, values);
 
-        ArrayList<Integer> values = new ArrayList<>(map.values());
-        Collections.sort(values, Collections.reverseOrder());
+    }
 
+    private static void printTheWordsInHighestOccurencesFirstOrder(Map<String, Integer> map, ArrayList<Integer> values) {
         for (int i = 0; i < 50; i++) {
             int value = values.get(i);
             String key = "";
@@ -46,7 +40,27 @@ public class Main {
                     "COMMON WORD <" + key + ">" +
                     " occurs " + value + " times");
         }
+    }
 
+    private static ArrayList<Integer> createSortedArrayListFromValuesOfHashMap(Map<String, Integer> map) {
+        ArrayList<Integer> values = new ArrayList<>(map.values());
+        Collections.sort(values, Collections.reverseOrder());
+        return values;
+    }
+
+    private static Map<String, Integer> constructMapFromWords(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            word = word.toUpperCase();
+            boolean containsKey = map.containsKey(word);
+            if (containsKey) {
+                map.replace(word, map.get(word) + ONE);
+            } else {
+                map.put(word, ONE);
+            }
+        }
+        return map;
     }
 }
 
